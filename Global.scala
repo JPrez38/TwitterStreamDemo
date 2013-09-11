37241be7a1c2ce381da52b1744bff2724f5f851c
Jacob Preston <jacobpreston4@gmail.com>
2013-09-11 00:03:35 -0400

    hopefully this works

--- Global.scala
+++ Global.scala
@@ -4,12 +4,18 @@ object Global extends GlobalSettings {
 
   override def onStart(app: Application) {
     Logger.info("Application has started")
-    println("hello")
+    import play.api.libs.concurrent.Akka
+    import akka.util.duration._
+    Akka.system(app).scheduler.schedule(10 seconds, 30 minutes){
+    	import models.SearchStreamer
+    	println("Worker process woke up")
+  		val search = Array("tapingo","Tapingo")
+  		SearchStreamer.run(search)
+    }
   }  
   
   override def onStop(app: Application) {
     Logger.info("Application shutdown...")
-    println("hello")
   }  
     
 }
