8e232d2b08939f4fbdbceb71d6d0dc023e3ab37f
Jacob Preston <jacobpreston4@gmail.com>
2013-10-04 17:47:10 -0400

    trying to remove retweets

--- app/models/Main.scala
+++ app/models/Main.scala
@@ -4,7 +4,7 @@ object Main {
 	println("hello")
 	def main(args: Array[String]){
 		println("Worker process woke up")
-  		val search = Array("tapingo","Tapingo")
+  		val search = Array("tapingo -RT","Tapingo -RT")
   		SearchStreamer.run(search)
     }
 }
