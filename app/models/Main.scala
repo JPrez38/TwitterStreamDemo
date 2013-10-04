fc3ab40149d94a89b3c15a868798dde9257d2c14
Jacob Preston <jacobpreston4@gmail.com>
2013-10-04 18:50:15 -0400

    updating

--- app/models/Main.scala
+++ app/models/Main.scala
@@ -4,7 +4,7 @@ object Main {
 	println("hello")
 	def main(args: Array[String]){
 		println("Worker process woke up")
-  		val search = Array("tech@nyu","Tech@nyu")
+  		val search = Array("tech@nyu","Tech@nyu","techatnyu","Techatnyu")
   		SearchStreamer.run(search)
     }
 }
